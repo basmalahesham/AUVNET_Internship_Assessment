733c0e6ae734f1cf071de3228059d925c80df1eb
basmalahesham <42021079@hti.edu.eg>
2025-06-16 02:27:25 +0300

    integrate it with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:auvnet_flutter_internship_assessment/core/routing/app_router.dart';
 import 'package:auvnet_flutter_internship_assessment/core/routing/routes.dart';
+import 'package:auvnet_flutter_internship_assessment/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ScreenUtilInit(
       builder: (context, child) => MyApp(appRouter: AppRouter()),
